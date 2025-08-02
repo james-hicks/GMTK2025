1435f530d0294776da2c9966046f24880de9a396
James-Hicks <james8hicks@gmail.com>
2025-08-01 22:39:24 -0400

    boomerang enemy states implemented

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,34 +1,141 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering.UI;
+using UnityEngine.AI;
+using UnityEngine.InputSystem.Android;
+using UnityEngine.XR;
 
 public class Enemy : MonoBehaviour, IHitable
 {
+    [Header("Enemy Stats")]
     public float HP;
+    public float viewRange = 20f;
+    public float attackRange = 7f;
+
 
     private bool dead = false;  
     private Animator animator;
 
     private float hitCooldown;
 
+    private Coroutine currentState;
+    [Space]
+    public string CurrentStateName;
+
+    private NavMeshAgent agent;
+    private Transform player;
+
     private void Awake()
     {
         animator = GetComponentInChildren<Animator>();
+        player = FindFirstObjectByType<PlayerController>().transform;
+        agent = GetComponent<NavMeshAgent>();
+
+        SwitchState(IdleState());
+    }
+
+    public void SwitchState(IEnumerator newState)
+    {
+        if(currentState != null)
+        {
+            StopCoroutine(currentState);
+        }
+        //Debug.Log("Switching to new State" + newState);
+
+        currentState = StartCoroutine(newState);
+    }
+
+    public IEnumerator IdleState()
+    {
+        CurrentStateName = "Idle";
+        animator.SetBool("Move", false);
+
+        while (true)
+        {
+            if(Vector3.Distance(transform.position, player.position) < viewRange)
+            {
+                SwitchState(ChaseState());
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public IEnumerator ChaseState()
+    {
+        CurrentStateName = "Chase";
+        animator.SetBool("Spin", false);
+        animator.SetBool("Move", true);
+        while (true)
+        {
+            agent.speed = 4f;
+
+            agent.isStopped = false;
+            agent.SetDestination(player.position);
+
+            if (Vector3.Distance(transform.position, player.position) < attackRange)
+            {
+                SwitchState(AttackState());
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public IEnumerator AttackState()
+    {
+        CurrentStateName = "Attack";
+        agent.isStopped = true;
+        animator.SetBool("Spin", true);
+        animator.SetBool("Move", false);
+        while (true)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(player.position);
+            agent.speed = 3f;
+
+            if (Vector3.Distance(transform.position, player.position) > attackRange + 1)
+            {
+                SwitchState(ChaseState());
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public IEnumerator HitState()
+    {
+        CurrentStateName = "Hit State";
+        animator.SetTrigger("Hit");
+
+        agent.ResetPath();
+        agent.isStopped = true;
+        agent.velocity = Vector3.zero;
+        animator.SetBool("Spin", false);
+        animator.SetBool("Move", false);
+
+        while (hitCooldown > 0)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+
+        SwitchState(IdleState());
     }
 
     public void GetHit(int damage)
     {
         if (dead || hitCooldown > 0) return;
 
-        hitCooldown = 0.5f;
-        HP -= damage;
 
+        HP -= damage;
+        hitCooldown = 0.5f;
         if (HP <= 0f)
         {
             Die();
         }
         else
         {
-            animator.SetTrigger("Hit");
+            Debug.Log("Hit");
+            SwitchState(HitState());
         }
     }
 
@@ -42,6 +149,8 @@ public class Enemy : MonoBehaviour, IHitable
 
     private void Die()
     {
+        StopAllCoroutines();
+        agent.isStopped = true;
         dead = true;
         animator.SetBool("Death", true);
         Destroy(gameObject, 5f);
